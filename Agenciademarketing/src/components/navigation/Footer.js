f40a7f0dfb0d93d2da7cca2fc114c6a3547b25b6
l18630 <121523910+l18630@users.noreply.github.com>
2023-01-04 21:39:42 -0300

    update

--- Agenciademarketing/src/components/navigation/Footer.js
+++ Agenciademarketing/src/components/navigation/Footer.js
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import logo_boomslag from 'assets/img/boomslag-black.png'
 
 const navigation = {
     solutions: [
@@ -8,17 +9,14 @@ const navigation = {
       { name: 'Insights', href: '#' },
     ],
     support: [
-      { name: 'Pricing', href: '#' },
-      { name: 'Documentation', href: '#' },
-      { name: 'Guides', href: '#' },
-      { name: 'API Status', href: '#' },
+      { name: 'Contacto', href: '/contacto' },
     ],
     company: [
-      { name: 'About', href: '#' },
-      { name: 'Blog', href: '#' },
-      { name: 'Jobs', href: '#' },
-      { name: 'Press', href: '#' },
-      { name: 'Partners', href: '#' },
+      { name: 'Casos', href: '/casos' },
+      { name: 'Servicios', href: '/servicios' },
+      { name: 'Nosotros', href: '/nosotros' },
+      { name: 'Carreras', href: '/carreras' },
+      { name: 'Blog', href: '/blog' },
     ],
     legal: [
       { name: 'Claim', href: '#' },
@@ -92,20 +90,16 @@ const navigation = {
 
 function Footer(){
     return(
-        <footer className="bg-white" aria-labelledby="footer-heading">
+        <footer className="bg-gray-50" aria-labelledby="footer-heading">
         <h2 id="footer-heading" className="sr-only">
           Footer
         </h2>
         <div className="mx-auto max-w-full py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
           <div className="xl:grid xl:grid-cols-3 xl:gap-8">
             <div className="space-y-8 xl:col-span-1">
-              <img
-                className="h-10"
-                src="https://tailwindui.com/img/logos/mark.svg?color=gray&shade=300"
-                alt="Company name"
-              />
+            <img src={logo_boomslag} width={180} height={90} className=""/>
               <p className="text-base text-gray-500">
-                Making the world a better place through constructing elegant hierarchies.
+                WEB3 COMMUNITY.
               </p>
               <div className="flex space-x-6">
                 {navigation.social.map((item) => (
@@ -119,7 +113,7 @@ function Footer(){
             <div className="mt-12 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
               <div className="md:grid md:grid-cols-2 md:gap-8">
                 <div>
-                  <h3 className="text-base font-medium text-gray-900">Solutions</h3>
+                  <h3 className="text-base font-medium text-gray-900">Visit Us</h3>
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.solutions.map((item) => (
                       <li key={item.name}>
@@ -131,7 +125,7 @@ function Footer(){
                   </ul>
                 </div>
                 <div className="mt-12 md:mt-0">
-                  <h3 className="text-base font-medium text-gray-900">Support</h3>
+                  <h3 className="text-base font-medium text-gray-900">Contact</h3>
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.support.map((item) => (
                       <li key={item.name}>
@@ -157,7 +151,7 @@ function Footer(){
                   </ul>
                 </div>
                 <div className="mt-12 md:mt-0">
-                  <h3 className="text-base font-medium text-gray-900">Legal</h3>
+                  <h3 className="text-base font-medium text-gray-900">Services</h3>
                   <ul role="list" className="mt-4 space-y-4">
                     {navigation.legal.map((item) => (
                       <li key={item.name}>
@@ -172,7 +166,7 @@ function Footer(){
             </div>
           </div>
           <div className="mt-12 border-t border-gray-200 pt-8">
-            <p className="text-base text-gray-400 xl:text-center">&copy; 2020 Your Company, Inc. All rights reserved.</p>
+            <p className="text-base text-gray-400 xl:text-center">&copy; FORK DAO</p>
           </div>
         </div>
       </footer>
--- Agenciademarketing/src/containers/pages/Home.jsx
+++ Agenciademarketing/src/containers/pages/Home.jsx
@@ -1,39 +1,4 @@
-/*import BlogList from "components/home/BlogList"
-import CTA from "components/home/CTA"
-import Features from "components/home/Features"
 import Header from "components/home/Header"
-import Incentives from "components/home/Incentives"
-import LogoCloud from "components/home/LogoCloud"
-import UseCases from "components/home/UseCases"
-import Footer from "components/navigation/Footer"
-import Navbar from "components/navigation/Navbar"
-import Scroll from "components/SmoothScrollbar"
-import Layout from "hocs/layouts/Layout"
-import { useEffect } from "react"
-
-
-function Home(){
-    useEffect(()=>{
-        window.scrollTo(0,0)
-    },[])
-    return(
-        <Layout>
-            <Navbar/>
-            <div data-scroll-section className="pt-28">
-                <Header/>
-                <Incentives/>
-                <UseCases/>
-                <Features/>
-                <CTA/>
-                <LogoCloud/>
-                <BlogList/>
-            </div>
-                <Footer/>
-        </Layout>
-    )
-}
-export default Home */
-
 import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/Layout"
@@ -43,7 +8,7 @@ function Home() {
         <Layout>
             <Navbar />
             <div className="pt-28">
-                Home
+                <Header/>
             </div>
             <Footer />
         </Layout>

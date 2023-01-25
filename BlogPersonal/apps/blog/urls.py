fd84f2a0485e8188fea891d830c81bcf6e64f978
l18630 <121523910+l18630@users.noreply.github.com>
2023-01-25 20:23:20 -0300

    update

--- BlogPersonal/apps/blog/urls.py
+++ BlogPersonal/apps/blog/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('list', BlogListView.as_view()),
     path('by_category', ListPostsByCategoryView.as_view()),
+    path('detail/<slug>', PostsDetailView.as_view()),
 ]
--- BlogPersonal/apps/blog/views.py
+++ BlogPersonal/apps/blog/views.py
@@ -33,11 +33,61 @@ class ListPostsByCategoryView(APIView):
         if Post.objects.all().exists():
             
             slug = request.query_params.get('slug')
-            print(slug)
+            category = Category.objects.get(slug=slug)
+
             posts = Post.objects.order_by('-published').all()
 
-            
-            print(posts)
-            return  Response({'success':'success'}, status=status.HTTP_200_OK)
+            #if category.parent:
+             # posts = posts.filter(category=category)
+            #else:
+            if not Category.objects.filter(parent=category).exists():
+                posts.filter(category=category)
+            else:
+                sub_categories = Category.objects.filter(parent=category)
+                
+                filtered_categories = [category]
+              
+                for cat in sub_categories:
+                    filtered_categories.append(cat)
+                 
+
+                filtered_categories = tuple(filtered_categories)
+
+                posts = posts.filter(category__in=filtered_categories)
+
+            paginator = SmallSetPagination()
+            results = paginator.paginate_queryset(posts, request)
+            serializer = PostListSerializer(results, many=True)
+
+
+
+            return paginator.get_paginated_response({'posts': serializer.data})
         else:
-             return Response({'error': 'No posts found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'No posts found'}, status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+class PostDetailView(APIView):
+    #permission_classes = (permissions.AllowAny,)
+    def get(self, request, slug, format=None):
+        if Post.objects.filter(slug=slug).exists():
+            
+            post = Post.objects.get(slug=slug)
+            serializer = PostSerializer(post)
+
+             address = request.META.get('HTTP_X_FORWARDED_FOR')
+            if address:
+                ip = address.split(',')[-1].strip()
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+
+            if not ViewCount.objects.filter(post=post, ip_address=ip):
+                view = ViewCount(post=post,ip_address=ip)
+                view.save()
+                post.views += 1
+                post.save()
+
+            return Response({'post':serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error':'Post doesnt exist'}, status=status.HTTP_404_NOT_FOUND)

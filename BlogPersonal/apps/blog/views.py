b94d3538c0b78bea92d9b9fa3be9fb5ba9ff2867
l18630 <121523910+l18630@users.noreply.github.com>
2023-01-26 20:48:12 -0300

    update

--- BlogPersonal/apps/blog/views.py
+++ BlogPersonal/apps/blog/views.py
@@ -9,12 +9,13 @@ from apps.category.models import Category
 from .serializers import PostSerializer, PostListSerializer
 from .pagination import SmallSetPagination, MediumSetPagination, LargeSetPagination
 
+from django.db.models.query_utils import Q
 class BlogListView(APIView):
     permission_classes = (permissions.AllowAny,)
     def get(self, request, format=None):
-        if Post.objects.all().exists():
+        if Post.postobjects.all().exists():
             
-            posts = Post.objects.all()
+            posts = Post.postobjects.all()
 
             paginator = SmallSetPagination()
             results = paginator.paginate_queryset(posts, request)
@@ -30,12 +31,12 @@ class BlogListView(APIView):
 class ListPostsByCategoryView(APIView):
     permission_classes = (permissions.AllowAny,)
     def get(self, request, format=None):
-        if Post.objects.all().exists():
+        if Post.postobjects.all().exists():
             
             slug = request.query_params.get('slug')
             category = Category.objects.get(slug=slug)
 
-            posts = Post.objects.order_by('-published').all()
+            posts = Post.postobjects.order_by('-published').all()
 
             #if category.parent:
              # posts = posts.filter(category=category)
@@ -69,11 +70,11 @@ class ListPostsByCategoryView(APIView):
 
 
 class PostDetailView(APIView):
-    #permission_classes = (permissions.AllowAny,)
+    permission_classes = (permissions.AllowAny,)
     def get(self, request, slug, format=None):
-        if Post.objects.filter(slug=slug).exists():
+        if Post.postobjects.filter(slug=slug).exists():
             
-            post = Post.objects.get(slug=slug)
+            post = Post.postobjects.get(slug=slug)
             serializer = PostSerializer(post)
 
             address = request.META.get('HTTP_X_FORWARDED_FOR')
@@ -91,3 +92,17 @@ class PostDetailView(APIView):
             return Response({'post':serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({'error':'Post doesnt exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    
+class SearchBlogView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    def get(self,request, format=None):
+        search_term = request.query_params.get('s')
+        matches = Post.postobjects.filter(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term) |
+            Q(category__name__icontains=search_term)
+        )
+
+        serializer = PostListSerializer(matches, many=True)
+        return Response({'filtered_posts': serializer.data}, status=status.HTTP_200_OK)
